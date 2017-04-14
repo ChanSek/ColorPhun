10f7e6879f9e5811da029f05d2bbca82b3254f1f
Chandra Sekhar Nayak <Chandra.Nayak@Symphonyteleca.com>
2017-04-15 00:45:43 +0530

    Step 4 :- Migrating the code to make it compatible with ChanseGames 1. Fixed all compilation errors in HomeScreenActivity cauesd due to migration

--- app/src/main/java/com/chanse/games/colorphun/HomeScreenActivity.java
+++ app/src/main/java/com/chanse/games/colorphun/HomeScreenActivity.java
@@ -8,20 +8,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.example.games.basegameutils.BaseGameActivity;
-import com.prakharme.prakharsriv.colorphun.R;
-
-public class HomeScreenActivity extends BaseGameActivity implements View.OnClickListener {
+public class HomeScreenActivity extends BaseGameActivity {
 
     private Button playGameButton;
-    private View signInButton, signOutButton;
     private ImageView logoView;
     private TextView taglineTextView1, taglineTextView2, taglineTextView3;
     SharedPreferences sharedPreferences;
@@ -30,11 +25,6 @@ public class HomeScreenActivity extends BaseGameActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-        signInButton = findViewById(R.id.sign_in_button);
-        signOutButton = findViewById(R.id.sign_out_button);
-        signInButton.setOnClickListener(this);
-        signOutButton.setOnClickListener(this);
 
         taglineTextView1 = (TextView) findViewById(R.id.tagline_text);
         taglineTextView2 = (TextView) findViewById(R.id.tagline_text2);
@@ -51,8 +41,6 @@ public class HomeScreenActivity extends BaseGameActivity implements View.OnClick
         // get user's preference for sign-in
         sharedPreferences = this.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        int shouldSignIn = sharedPreferences.getInt("SIGNIN", 1);
-        getGameHelper().setMaxAutoSignInAttempts(shouldSignIn);
     }
 
 
@@ -100,13 +88,10 @@ public class HomeScreenActivity extends BaseGameActivity implements View.OnClick
 
     ValueAnimator getFadeAnimator() {
         ValueAnimator anim = ValueAnimator.ofFloat(0f, 1f);
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                taglineTextView1.setAlpha(valueAnimator.getAnimatedFraction());
-                taglineTextView2.setAlpha(valueAnimator.getAnimatedFraction());
-                taglineTextView3.setAlpha(valueAnimator.getAnimatedFraction());
-            }
+        anim.addUpdateListener(valueAnimator -> {
+            taglineTextView1.setAlpha(valueAnimator.getAnimatedFraction());
+            taglineTextView2.setAlpha(valueAnimator.getAnimatedFraction());
+            taglineTextView3.setAlpha(valueAnimator.getAnimatedFraction());
         });
         return anim;
     }
@@ -114,17 +99,14 @@ public class HomeScreenActivity extends BaseGameActivity implements View.OnClick
     ValueAnimator getBounceAnimator() {
         ValueAnimator anim = ValueAnimator.ofFloat(0f, 1f);
         anim.setInterpolator(new BounceInterpolator());
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                logoView.setScaleX(valueAnimator.getAnimatedFraction());
-                logoView.setScaleY(valueAnimator.getAnimatedFraction());
-                logoView.setAlpha(valueAnimator.getAnimatedFraction());
+        anim.addUpdateListener(valueAnimator -> {
+            logoView.setScaleX(valueAnimator.getAnimatedFraction());
+            logoView.setScaleY(valueAnimator.getAnimatedFraction());
+            logoView.setAlpha(valueAnimator.getAnimatedFraction());
 
-                playGameButton.setScaleX(valueAnimator.getAnimatedFraction());
-                playGameButton.setScaleY(valueAnimator.getAnimatedFraction());
-                playGameButton.setAlpha(valueAnimator.getAnimatedFraction());
-            }
+            playGameButton.setScaleX(valueAnimator.getAnimatedFraction());
+            playGameButton.setScaleY(valueAnimator.getAnimatedFraction());
+            playGameButton.setAlpha(valueAnimator.getAnimatedFraction());
         });
         return anim;
     }
@@ -132,36 +114,5 @@ public class HomeScreenActivity extends BaseGameActivity implements View.OnClick
     public void playGame(View view) {
         startActivity(new Intent(this, EasyGameActivity.class));
         //startActivity(new Intent(this, HardGameActivity.class));
-    }
-
-    // TODO: Find out the best practices for below events and handle them
-    @Override
-    public void onSignInFailed() {
-        signInButton.setVisibility(View.VISIBLE);
-        signOutButton.setVisibility(View.GONE);
-        Log.e("SIGN IN", "ERROR Signin in home screen");
-    }
-
-    @Override
-    public void onSignInSucceeded() {
-        signInButton.setVisibility(View.GONE);
-        signOutButton.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.sign_in_button) {
-            beginUserInitiatedSignIn();
-        } else if (view.getId() == R.id.sign_out_button) {
-            signOut();
-
-            // save user's preference for sign-in
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putInt("SIGNIN", 0);
-            editor.apply();
-
-            signInButton.setVisibility(View.VISIBLE);
-            signOutButton.setVisibility(View.GONE);
-        }
     }
 }
